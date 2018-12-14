a910d946f237fdef1130964284ba2c8c0c1cd5e9
dsurov <d-surov@yandex.ru>
2018-12-15 01:26:33 +0300

    homework 3.3.1

--- app/src/main/java/ru/geekbrains/android1/weather/Activity2.java
+++ app/src/main/java/ru/geekbrains/android1/weather/Activity2.java
@@ -1,7 +1,12 @@
 package ru.geekbrains.android1.weather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -9,5 +14,43 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Content();
+
+    }
+
+    private void Content()
+    {
+        final LinearLayout LL2 = (LinearLayout) findViewById(R.id.InnerRelativeLayout9);
+        final LinearLayout LL3 = (LinearLayout) findViewById(R.id.InnerRelativeLayout7);
+        final LinearLayout LL4 = (LinearLayout) findViewById(R.id.InnerRelativeLayout8);
+        final TextView city = (TextView) findViewById(R.id.city1);
+        Intent intent = getIntent();
+        city.setText(intent.getStringExtra("City"));
+        boolean b1 = intent.getBooleanExtra("chk1bx", false);
+        if (b1 == true) {
+            LL2.setVisibility(View.VISIBLE);
+        }
+        boolean b2 = intent.getBooleanExtra("chk2bx", false);
+        if (b2 == true) {
+            LL3.setVisibility(View.VISIBLE);
+        }
+        boolean b3 = intent.getBooleanExtra("chk3bx", false);
+        if (b3 == true) {
+            LL4.setVisibility(View.VISIBLE);
+        }
+
+        Button btnBack = (Button) findViewById(R.id.back_button);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent firstAct = new Intent(getApplicationContext(), MainActivity.class);
+                final String str2 = city.getText().toString();
+                firstAct.putExtra("City2", str2);
+                firstAct.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(firstAct);
+            }
+        });
+
     }
 }
--- app/src/main/java/ru/geekbrains/android1/weather/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1/weather/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.android1.weather;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,7 +8,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
@@ -19,43 +25,15 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    String[] citys;
-
+    protected String[] citys;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         makeInfo("onCreate");
-        greeting_user();
-
-        ListView lView = (ListView) findViewById(R.id.lview);
-        citys = getResources().getStringArray(R.array.city_list);
-
-        // создаем arrаy-адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, citys);
-
-        // присваиваем адаптер списку
-        lView.setAdapter(adapter);
-
-        lView.setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1,
-                                    int pos, long id
-            ) {
-
-                Toast toast = Toast.makeText(
-                        getApplicationContext(), citys[pos], Toast.LENGTH_SHORT
-                );
-                toast.show();
-            }
-
-        });
+        Content();
     }
-
 
     @Override
     protected void onStart() {
@@ -104,28 +82,113 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         //Выводим в logCat каждый LifeCycle
         Log.d(TAG, text);
-
     }
 
-    private void greeting_user() {
+    private void Content()
+
+    {
+        final TextView greeting1 = (TextView) findViewById(R.id.greeting);
+        final ListView Lview1 = (ListView) findViewById(R.id.lview);
+        final FrameLayout FRM = (FrameLayout) findViewById(R.id.FRM1);
+        citys = getResources().getStringArray(R.array.city_list);
+        final LinearLayout LL1 = (LinearLayout) findViewById(R.id.InnerRelativeLayout);
+        final TextView LsT = (TextView) findViewById(R.id.list_city_text);
+        final TextView list_city_text = (TextView) findViewById(R.id.list_city_text);
+        final Button change_city_button2 = (Button) findViewById(R.id.change_city_button1);
+        final Button exit_button1 = (Button) findViewById(R.id.exit_button);
+        final Button OkBtn = (Button) findViewById(R.id.ok_button);
+        final Button chkBx1 = (CheckBox) findViewById(R.id.checkBoxBold1);
+        final Button chkBx2 = (CheckBox) findViewById(R.id.checkBoxBold2);
+        final Button chkBx3 = (CheckBox) findViewById(R.id.checkBoxBold3);
+
+        greeting1.setText(greeting_user());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, citys);
+        Lview1.setAdapter(adapter);
+        Lview1.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int pos, long id
+            ) {
+
+                list_city_text.setText(citys[pos]);
+                Lview1.setVisibility(View.GONE);
+                FRM.setVisibility(View.GONE);
+                LL1.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        //кнопка ВЫХОД
+        exit_button1.setOnClickListener(new View.OnClickListener() { // Обработк нажатий
+            @Override
+            public void onClick(View v) {
+                makeInfo("exit_button");
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+
+        });
+
+        //кнопка выбора города
+        change_city_button2.setOnClickListener(new View.OnClickListener() { // Обработк нажатий
+            @Override
+            public void onClick(View v) {
+                makeInfo("Button Click");
+                LL1.setVisibility(View.GONE);
+                Lview1.setVisibility(View.VISIBLE);
+                FRM.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        //кнопка ОК
+        OkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent secAct2 = new Intent(getApplicationContext(),
+                        Activity2.class);
+                final boolean chk1 = ((CheckBox) chkBx1).isChecked();
+                final boolean chk2 = ((CheckBox) chkBx2).isChecked();
+                final boolean chk3 = ((CheckBox) chkBx3).isChecked();
+                final String str2 = LsT.getText().toString();
+                secAct2.putExtra("City", str2);
+                secAct2.putExtra("chk1bx", chk1);
+                secAct2.putExtra("chk2bx", chk2);
+                secAct2.putExtra("chk3bx", chk3);
+                startActivity(secAct2);
+            }
+        });
+
+        Intent intent2 = getIntent();
+        if (intent2.getStringExtra("City2") != null) {
+            LsT.setText(intent2.getStringExtra("City2"));
+        }
+    }
+
+    private String greeting_user() {
+        String usr_greeting = getString(R.string.greeting6);
         // Текущее время
         Date currentDate = new Date();
-        // Форматирование времени как "часы:минуты:секунды"
+        // Форматирование времени как "часы"
         DateFormat timeFormat = new SimpleDateFormat("HH", Locale.getDefault());
         String timeText = timeFormat.format(currentDate);
         int i = Integer.parseInt(timeText);
-        Toast.makeText(getApplicationContext(), timeText, Toast.LENGTH_SHORT).show();
         if (i >= 05 && i < 12) {
-            Toast.makeText(getApplicationContext(), "Доброе утро", Toast.LENGTH_SHORT).show();
+            usr_greeting = getString(R.string.greeting1);
         }
         if (i >= 12 && i <= 15) {
-            Toast.makeText(getApplicationContext(), "Добрый день", Toast.LENGTH_SHORT).show();
+            usr_greeting = getString(R.string.greeting2);
         }
-        if (i > 16 && i <= 23) {
-            Toast.makeText(getApplicationContext(), "Добрый вечер", Toast.LENGTH_SHORT).show();
+        if (i > 15 && i <= 23) {
+            usr_greeting = getString(R.string.greeting3);
         }
         if (i >= 00 && i <= 04) {
-            Toast.makeText(getApplicationContext(), "Доброй ночи", Toast.LENGTH_SHORT).show();
+            usr_greeting = getString(R.string.greeting5);
         }
+        return usr_greeting;
     }
+
+
 }
